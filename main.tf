8fba6a7f631782679e9435b1f6d6128c98c4c2c0
Atanas Chuchulev <atanas.v4@gmail.com>
2021-07-20 13:34:44 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,3 +14,12 @@ resource "tfe_workspace" "test" {
   name         = "my-new-tfc-workspace"
   organization = var.my-org-name
 }
+
+resource "null_resource" "test" {
+  triggers = {
+    id = uuid()
+  }
+  provisioner "local-exec" {
+    command = "echo $TFE_TOKEN"
+  }
+}
